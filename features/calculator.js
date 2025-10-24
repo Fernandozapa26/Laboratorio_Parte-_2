f359d8e41a4d03fc5c938ecde1357e66d04abcd0
FERNANDO_JOSE_ZAPA_PORTILLO <fzapaportillo@gmail.com>
2025-10-24 07:29:37 -0500

    feat(calc): add sum function

---
+++ features/calculator.js
@@ -0,0 +1,10 @@
+ 
+// crear funcion de suma
+
+function sumar(numero1, numero2) {
+    let resultado = numero1 + numero2;
+    return resultado;
+  }
+  
+  console.log(sumar(100, 5)); // probando la funcion
+  
